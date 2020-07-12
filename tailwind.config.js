ab71f608bfc86242a9be183d8860343c7d2a972f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2020-07-11 19:50:45 -0700

    feat(*): add package and configuration
    set up the live reload server and add the basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* 
+  Config file and dev dependency for Tailwind are present 
+  purely to enable intellisense, and are not for anything else
+*/
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
